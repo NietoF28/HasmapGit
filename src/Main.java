aa12d843fb62eff813a3b2f6ebcafcde92ad6bab
Andrés Cárdenas <Estebancardenas777@gmail.com>
2024-10-09 17:48:53 -0500

    Completando el main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Tienda tienda = new Tienda();
+        Scanner teclado = new Scanner(System.in);
+//       Duvan felipe nieto hernandez id 92736
+
+        Producto producto1 = new Producto("001", "Laptop", 1500.0);
+        Producto producto2 = new Producto("002", "Smartphone", 800.0);
+        Producto producto3 = new Producto("003", "Tablet", 300.0);
+
+
+        tienda.agregarProducto(producto1);
+        tienda.agregarProducto(producto2);
+        tienda.agregarProducto(producto3);
+
+
     }
 }
