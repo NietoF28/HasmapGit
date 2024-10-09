cab909db5bdcbaf53567cd236a974af0ab022eb5
Your Name <you@example.com>
2024-10-09 17:48:25 -0500

    Relleno clase main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Productos en la tienda:");
+        tienda.mostrarProductos();
+
+        System.out.println("\nBusca el producto que necesite:");
+
+        Producto buscado = tienda.buscarProducto(teclado.nextLine());
+        if (buscado != null) {
+            System.out.println(buscado);
+        } else {
+            System.out.println("Producto no encontrado.");
+        }
+
+
     }
+}
+    }
+
 }
